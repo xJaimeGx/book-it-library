8ed57f1235cb114438800046b51d21e6cf673b5f
xJaimeGx <beanieman019@protonmail.com>
2022-05-22 15:26:14 -0500

    finish front end

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,7 +7,51 @@ const resolvers = {
         const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
         return userData;
       }
-      throw new AuthenticationError('You need to be logged in!');
+      throw new AuthenticationError('You must login first.');
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('User not found');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Wrong credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+
+    },
+    saveBook: async (parent, { newBook }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: newBook }},
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('You must login first.');
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId }}},
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('You must login first.');
     },
   },
 };
