25432913160f0af3143e1e2946615871444e5a20
xJaimeGx <beanieman019@protonmail.com>
2022-05-22 09:53:11 -0500

    add remaining typeDefs and test queries

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,15 @@
+const { User } = require('../models');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+        return userData;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+};
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,7 +8,35 @@ const typeDefs = gql`
     bookCount: Int
     savedBooks: [Book]
   }
-  
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  input InputBook {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+  }
+  type Query {
+    me: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(newBook: InputBook!): User
+    removeBook(bookId: ID!): User
+  }
 `;
 
 module.exports = typeDefs;
